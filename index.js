ee3418f7332bd0fe777e2b886529511452262a4c
CaseyRayson1995 <caseyrayson1995@gmail.com>
2024-09-12 23:46:38 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
+    //*binds an event listener in addingEventListener() code below
+    const input = document.getElementById('button');
+
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+    
+    input.addEventListener('click', clickAlert);
+
 }
+  
+
